f105eb3fde7265c7e3b907488b702ae9d8647db8
userquin <userquin@gmail.com>
2024-07-26 15:55:25 +0200

    chore: add react and react-ssr examples

---
+++ examples/react-ssr/src/root.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useRegisterSW } from 'virtual:pwa-register/react'
+
+export default function Root() {
+    const [count, setCount] = React.useState(0)
+
+    if (typeof navigator !== 'undefined')
+        useRegisterSW({ immediate: true })
+
+    const [hydrated, setHydrated] = React.useState(false)
+    React.useEffect(() => {
+        setHydrated(true)
+    }, [])
+
+    return (
+        <div>
+            <div>hydrated: {String(hydrated)}</div>
+            <div>Count: {count}</div>
+            <button onClick={() => setCount((v) => v - 1)}>-1</button>
+            <button onClick={() => setCount((v) => v + 1)}>+1</button>
+        </div>
+    )
+}
---
+++ examples/react/vite.config.ts
@@ -0,0 +1,86 @@
+import process from 'node:process'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import type { ManifestOptions, VitePWAOptions } from '../../server'
+import { VitePWA } from '../../server'
+import replace from '@rollup/plugin-replace'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'PWA Router',
+    short_name: 'PWA Router',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest'
+  ;(pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest'
+  ;(pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject'
+  pwaOptions.injectManifest = {
+    minify: false,
+    enableWorkboxModulesLogs: true,
+  }
+}
+
+if (claims)
+  pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying)
+  pwaOptions.selfDestroying = selfDestroying
+
+export default defineConfig({
+  server: {
+    fs: {
+      allow: ['.', '../../server']
+    }
+  },
+  // base: process.env.BASE_URL || 'https://github.com/',
+  build: {
+    sourcemap: process.env.SOURCE_MAP === 'true',
+  },
+  plugins: [
+    react(),
+    VitePWA(pwaOptions),
+    replace(replaceOptions),
+  ],
+})
